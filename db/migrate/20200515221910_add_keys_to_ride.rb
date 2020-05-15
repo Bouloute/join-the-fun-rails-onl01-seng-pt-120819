b864cdf0e165516e51f5ce05552a447cc7413709
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-05-15 22:23:42 +0000

    Automatically backed up by Learn

--- db/migrate/20200515221910_add_keys_to_ride.rb
+++ db/migrate/20200515221910_add_keys_to_ride.rb
@@ -1,6 +1,6 @@
 class AddKeysToRide < ActiveRecord::Migration[5.0]
   def change
-    add_column :rides, :passenger, :belongs_to
-    add_column :rides, :taxi, :belongs_to
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :belongs_to
   end
 end
